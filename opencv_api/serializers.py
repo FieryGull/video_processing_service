a5b81b8deb208e81f7bdf84d40b8283a05ce1fbc
Vitaly Chaykin <v.chaykin@3divi.com>
2022-09-06 16:18:17 +0500

    Refine serializers

--- opencv_api/serializers.py
+++ opencv_api/serializers.py
@@ -1,14 +1,31 @@
 from rest_framework import serializers
-from .models import VideoUploadModel
 
 
-class VideoSerializer(serializers.ModelSerializer):
-    # id = serializers.IntegerField()
-    # status = serializers.CharField()
-    # current_progress = serializers.IntegerField()
-    # faces_count = serializers.IntegerField()
-    class Meta:
-        model = VideoUploadModel
-        fields = ['id', 'status', 'current_progress', 'faces_count']
-        extra_kwargs = {'result_aggregation': 1}
+class VideoSerializer(serializers.Serializer):
+    id = serializers.IntegerField()
+    status = serializers.CharField()
+    current_progress = serializers.IntegerField()
+    faces_count = serializers.IntegerField()
 
+    def to_representation(self, instance):
+        return {
+            'id': str(instance.id),
+            'status': instance.status,
+            'current_progress': instance.current_progress,
+            'result_aggregation': {
+                'faces_count': instance.faces_count,
+            }
+        }
+
+
+class IdVideoSerializer(serializers.Serializer):
+    id = serializers.IntegerField()
+
+    def to_representation(self, instance):
+        return {
+            'video_id': str(instance.id),
+        }
+
+
+class ErrorSerializer(serializers.Serializer):
+    error = serializers.CharField()
