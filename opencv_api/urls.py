348a66af94c9a3c72042fb89f3d77fb11d750ce0
Vitaly Chaykin <v.chaykin@3divi.com>
2022-09-06 16:14:59 +0500

    Update UploadVideo and CancelUploadVideo endpoints

--- opencv_api/urls.py
+++ opencv_api/urls.py
@@ -1,12 +1,10 @@
-from django.conf.urls import url
 from django.urls import path
-
 from . import views
-from django.conf import settings
-from django.conf.urls.static import static
+
 
 urlpatterns = [
-    path('vdface/', views.vdface_view, name='vdface'),
     path('', views.main_view, name='main'),
-    path('processing/video/', views.UploadVideoView.as_view(), name='video')
+    path('processing/video/', views.UploadVideoView.as_view(), name='video'),
+    path('processing/video/<int:pk>/cancel', views.CancelUploadVideoView.as_view(), name='video')
+
 ]
--- opencv_api/views.py
+++ opencv_api/views.py
@@ -1,50 +1,64 @@
-from django.shortcuts import render
-from .forms import VideoUploadForm
-from django.conf import settings
-from asgiref.sync import sync_to_async
-from .vdface_algorithm import opencv_detect
+import io
 import requests
+
+from django.shortcuts import render
+from rest_framework import status
+from rest_framework.status import HTTP_400_BAD_REQUEST
+from rest_framework.parsers import JSONParser
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from opencv_api import tasks
+from django.db import Error
+from .models import VideoUploadModel
+from .serializers import VideoSerializer, IdVideoSerializer, ErrorSerializer
+from .forms import VideoUploadForm
 
 
 async def main_view(request):
-    return render(request, 'opencv_api/main.html', {})
-
-
-def vdface_view(request):
-    # loop = asyncio.get_event_loop()
-    form = VideoUploadForm(request.POST, request.FILES)
-    if request.method == 'POST':
-        if form.is_valid():
-            post = form.save(commit=False)
-            post.save()
-            videoURL = settings.MEDIA_URL + form.instance.document.name
-            # loop.create_task(opencv_detect(settings.MEDIA_ROOT_URL + videoURL))
-            d = opencv_detect(settings.MEDIA_ROOT_URL + videoURL, form.instance.document.name).copy()
-            return render(request, 'opencv_api/vdface.html', {'form': form,
-                                                              'post': post,
-                                                              'current_progress': d['current_progress'],
-                                                              'faces_count': d['faces_count']})
-    else:
-        form = VideoUploadForm()
-    return render(request, 'opencv_api/vdface.html', {'form': form})
-
-
-from rest_framework.response import Response
-from rest_framework.views import APIView
-from .models import VideoUploadModel
-from .serializers import VideoSerializer
+    form = VideoUploadForm()
+    try:
+        videos = requests.get('http://127.0.0.1:8000/processing/video/')
+        stream = io.BytesIO(videos.content)
+        data = JSONParser().parse(stream)['videos']
+        return render(request, 'opencv_api/main.html', {'form': form, 'data': data})
+    except Exception:
+        pass
+    return render(request, 'opencv_api/main.html', {'form': form})
 
 
 class UploadVideoView(APIView):
     def get(self, request):
-        videos = VideoUploadModel.objects.all()
-        serializer = VideoSerializer(videos, many=True)
-        return Response({"videos": serializer.data})
+        try:
+            videos = VideoUploadModel.objects.all()
+            serializer = VideoSerializer(videos, many=True)
+            return Response({"videos": serializer.data})
+        except Exception:
+            error = 'Произошла ошибка получения видео'
+            error_serializer = ErrorSerializer(error)
+            return Response(error_serializer.data[0], status=HTTP_400_BAD_REQUEST)
 
     def post(self, request):
-        pass
+        try:
+            form = VideoUploadForm(request.POST, request.FILES)
+            if form.is_valid():
+                post = form.save(commit=False)
+                post.save()
+                tasks.process_video.delay(post.id)
+                video = VideoUploadModel.objects.filter(id=post.id)
+                serializer = IdVideoSerializer(video, many=True)
+                return Response(serializer.data[0])
+        except Exception:
+            error = 'Произошла ошибка загрузки видео'
+            error_serializer = ErrorSerializer(error)
+            return Response(error_serializer.data[0], status=HTTP_400_BAD_REQUEST)
 
 
 class CancelUploadVideoView(APIView):
-    def post(self, request):
-        pass
+    def post(self, request, pk):
+        try:
+            VideoUploadModel.objects.filter(id=pk).update(status="canceled")
+            return Response(status=status.HTTP_200_OK)
+        except Error:
+            error = 'Произошла ошибка отмены обработки'
+            error_serializer = ErrorSerializer(error)
+            return Response(error_serializer.data[0], status=HTTP_400_BAD_REQUEST)
